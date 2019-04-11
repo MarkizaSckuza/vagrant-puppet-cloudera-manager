6f5cc92ad25f553992886aeee01a9f762fff346f
MarkizaSckuza <margaritaq@ukr.net>
2019-04-11 15:22:19 +0300

    changed files location

--- puppet/modules/mysql/init.pp
+++ puppet/modules/mysql/init.pp
@@ -2,8 +2,25 @@ class mysql {
   
   $mysql_password = "qedw"
 
-  package { 'mysql-server': ensure => installed }
-  package { 'mysql': ensure => installed }
+  exec { 'wget_mysql':
+    command => "/usr/bin/wget http://repo.mysql.com/mysql-community-release-el7-5.noarch.rpm",
+  }
+
+  exec { 'rpm_mysql':
+    require => Exec['wget_mysql'],
+      command => "/usr/bin/rpm -ivh mysql-community-release-el7-5.noarch.rpm",
+  }
+
+  exec { 'yum_update':
+    require => Exec['rpm_mysql'],
+    command => '/usr/bin/yum -y update'
+  }
+
+  package { 'mysql-server':
+    require => Exec['yum_update'],
+    ensure => installed,
+    provider => 'yum',
+  }
 
   service { 'mysqld':
     enable => true,
@@ -11,26 +28,19 @@ class mysql {
     require => Package['mysql-server'],
   }
 
-  # file { "/var/lib/mysql/my.cnf":
-  #   owner => "mysql", group => "mysql",
-  #   source => "puppet:///mysql/my.cnf",
-  #   notify => Service["mysqld"],
-  #   require => Package["mysql-server"],
-  # }
- 
-  # file { "/etc/my.cnf":
-  #   require => File["/var/lib/mysql/my.cnf"],
-  #   ensure => "/var/lib/mysql/my.cnf",
-  # }
-
-  exec { 'set-mysql-password':
-    unless => "mysqladmin -uroot -p$mysql_password status",
+  exec { 'mysql_secure_installiation':
     path => ["/bin", "/usr/bin"],
-    command => "mysqladmin -uroot password $mysql_password",
+    command => "mysql --user=root -e \"
+    UPDATE mysql.user SET Password=PASSWORD('${mysql_password}') WHERE User='root';
+    DELETE FROM mysql.user WHERE User='';
+    GRANT ALL PRIVILEGES ON *.* TO 'root'@'cm\.node[0-9]\.com' IDENTIFIED BY '${mysql_password}' WITH GRANT OPTION;
+    DROP DATABASE IF EXISTS test;
+    DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
+    FLUSH PRIVILEGES;\"",
     require => Service['mysqld'],
   }
 
-  exec { 'create-db':
+  exec { 'create_db':
       command => "/usr/bin/mysql -uroot -p$mysql_password -e \"CREATE DATABASE scm DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;
                                                                GRANT ALL ON scm.* TO 'scm'@'%' IDENTIFIED BY 'scm';
 
@@ -45,11 +55,6 @@ class mysql {
 
                                                                CREATE DATABASE oozie DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;
                                                                GRANT ALL ON oozie.* TO 'oozie'@'%' IDENTIFIED BY 'oozie123';\"",
-      require => Service['mysqld', 'set-mysql-password'],
+      require => [Service['mysqld'], Exec['mysql_secure_installiation']],
     }
-
-    exec { 'prepare_cloudera_db':
-        command => "/opt/cloudera/cm/schema/scm_prepare_database.sh mysql scm scm scm",
-        require => Exec['create-db']
-    }
-}
+  }
